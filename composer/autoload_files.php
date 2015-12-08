ea798769b7af261e103a4411f973ccc5472405a5
James D. Forrester <jforrester@wikimedia.org>
2015-12-08 13:45:50 -0800

    Update OOjs UI to v0.14.1
    Release notes:
     https://git.wikimedia.org/blob/oojs%2Fui.git/v0.14.0/History.md
    
    Change-Id: Id0642023abdae574e32620fc0843631d86bae006

--- composer/autoload_files.php
+++ composer/autoload_files.php
@@ -6,7 +6,7 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = $vendorDir;
 
 return array(
-    '1bfdedaa1281e04163b722d52ddb3917' => $vendorDir . '/mediawiki/at-ease/src/Functions.php',
-    '926ff73ba5c430af40e0c80eaa99d7fd' => $vendorDir . '/wikimedia/relpath/src/RelPath.php',
-    '69e00fd5e49e5ddfcb2d5c9a210ada38' => $vendorDir . '/wikimedia/base-convert/src/Functions.php',
+    'a24b5daa493ecb5e767c7d1592a8da36' => $vendorDir . '/mediawiki/at-ease/src/Functions.php',
+    'd55c27a601de788b19a09b7d057d07ae' => $vendorDir . '/wikimedia/relpath/src/RelPath.php',
+    'c3f7f0e81464740a933532e81fa26cc7' => $vendorDir . '/wikimedia/base-convert/src/Functions.php',
 );
