071666ebe00a76bc40e3f2c6bb2af3615ba2b600
Reedy <reedy@wikimedia.org>
2017-03-30 02:14:21 +0100

    Update pear/pear-core-minimal from 1.10.1 to 1.10.3
    https://github.com/pear/pear-core-minimal/releases/tag/v1.10.2
    https://github.com/pear/pear-core-minimal/releases/tag/v1.10.3
    
    https://github.com/pear/pear-core-minimal/compare/v1.10.1...v1.10.3
    
    Change-Id: Id5f6eeabf9117cc0e89bfe139aac5797e5895530

--- composer/autoload_namespaces.php
+++ composer/autoload_namespaces.php
@@ -16,5 +16,5 @@ return array(
     'Less' => array($vendorDir . '/oyejorge/less.php/lib'),
     'Kafka\\' => array($vendorDir . '/nmred/kafka-php/src'),
     'Console' => array($vendorDir . '/pear/console_getopt'),
-    '' => array($vendorDir . '/pear/pear-core-minimal/src', $vendorDir . '/cssjanus/cssjanus/src'),
+    '' => array($vendorDir . '/cssjanus/cssjanus/src', $vendorDir . '/pear/pear-core-minimal/src'),
 );
