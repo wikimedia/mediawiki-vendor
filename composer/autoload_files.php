7cdb2ecd2be53a9e41d30303fa91f3c5fefb49f0
addshore <addshorewiki@gmail.com>
2017-08-09 15:41:52 -0400

    Wikibase: "data-values/geo": "2.1.1"
    Change-Id: I6540ba028a2cefb281e384089a0cda55e89d5700

--- composer/autoload_files.php
+++ composer/autoload_files.php
@@ -7,14 +7,15 @@ $baseDir = $vendorDir;
 
 return array(
     '7cb394c3af2b1ae832979b0368e0da62' => $vendorDir . '/data-values/data-values/DataValues.php',
-    'a24b5daa493ecb5e767c7d1592a8da36' => $vendorDir . '/mediawiki/at-ease/src/Functions.php',
     'd1715cacc3c23b16a030645514266a76' => $vendorDir . '/data-values/interfaces/Interfaces.php',
-    'f4c767faab43077fd89ff7cf99ffb4b8' => $vendorDir . '/serialization/serialization/Serialization.php',
+    'a24b5daa493ecb5e767c7d1592a8da36' => $vendorDir . '/mediawiki/at-ease/src/Functions.php',
     '90559502573a0d473dc66fde5c0ff7e2' => $vendorDir . '/data-values/common/Common.php',
+    'f4c767faab43077fd89ff7cf99ffb4b8' => $vendorDir . '/serialization/serialization/Serialization.php',
     'c3f7f0e81464740a933532e81fa26cc7' => $vendorDir . '/wikimedia/base-convert/src/Functions.php',
     'c50606d667a3fde2b80a955639479d3d' => $vendorDir . '/wikimedia/timestamp/src/defines.php',
     '04c6c5c2f7095ccf6c481d3e53e1776f' => $vendorDir . '/mustangostang/spyc/Spyc.php',
     'd55c27a601de788b19a09b7d057d07ae' => $vendorDir . '/wikimedia/relpath/src/RelPath.php',
     '3ef87127dc6892a0a78f223558a0b940' => $vendorDir . '/diff/diff/Diff.php',
     'c15f8000750e4e04ba559d9745044427' => $vendorDir . '/data-values/number/Number.php',
+    '5a494680c593293bd6035e42e2a6825c' => $vendorDir . '/data-values/geo/Geo.php',
 );
