c77d669ec71c7ea740ead9af743d743b0897ef3c
Leszek Manicki <leszek.manicki@wikimedia.de>
2017-12-05 13:31:10 +0100

    Remove data-values/data-types
    To be merged together with
    I6292331d945ef7737fdc2130001533212605c1ff.
    
    Bug: T180454
    Change-Id: I6791b11c0af6c81fbb6086dab85c59230dd2d2e4

--- composer/autoload_static.php
+++ composer/autoload_static.php
@@ -98,8 +98,6 @@ class ComposerStaticInit_mediawiki_vendor
             'DataValues\\Geo\\' => 15,
             'DataValues\\Deserializers\\' => 25,
             'DataValues\\' => 11,
-            'DataTypes\\Tests\\' => 16,
-            'DataTypes\\' => 10,
         ),
         'C' => 
         array (
@@ -255,14 +253,6 @@ class ComposerStaticInit_mediawiki_vendor
         'DataValues\\' => 
         array (
             0 => __DIR__ . '/..' . '/data-values/common/src/DataValues',
-        ),
-        'DataTypes\\Tests\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/data-values/data-types/tests',
-        ),
-        'DataTypes\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/data-values/data-types/src',
         ),
         'Composer\\Semver\\' => 
         array (
@@ -446,8 +436,6 @@ class ComposerStaticInit_mediawiki_vendor
         'Composer\\Semver\\VersionParser' => __DIR__ . '/..' . '/composer/semver/src/VersionParser.php',
         'Console_Getopt' => __DIR__ . '/..' . '/pear/console_getopt/Console/Getopt.php',
         'Copyable' => __DIR__ . '/..' . '/data-values/data-values/src/interfaces/Copyable.php',
-        'DataTypes\\DataType' => __DIR__ . '/..' . '/data-values/data-types/src/DataType.php',
-        'DataTypes\\DataTypeFactory' => __DIR__ . '/..' . '/data-values/data-types/src/DataTypeFactory.php',
         'DataValues\\BooleanValue' => __DIR__ . '/..' . '/data-values/data-values/src/DataValues/BooleanValue.php',
         'DataValues\\DataValue' => __DIR__ . '/..' . '/data-values/data-values/src/DataValues/DataValue.php',
         'DataValues\\DataValueObject' => __DIR__ . '/..' . '/data-values/data-values/src/DataValues/DataValueObject.php',
