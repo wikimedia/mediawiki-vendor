6e2b4e8f2dc2e470e5a40b40594b906f71bbbf37
Reedy <reedy@wikimedia.org>
2020-08-03 00:36:42 +0100

    Updating nikic/php-parser (v4.4.0 => v4.9.1)
    Change-Id: Ic03d226d8585ec91655293f6aee14d7094eb1d6d

--- nikic/php-parser/lib/PhpParser/Lexer/TokenEmulator/NullsafeTokenEmulator.php
+++ nikic/php-parser/lib/PhpParser/Lexer/TokenEmulator/NullsafeTokenEmulator.php
@@ -4,16 +4,16 @@ namespace PhpParser\Lexer\TokenEmulator;
 
 use PhpParser\Lexer\Emulative;
 
-final class CoaleseEqualTokenEmulator implements TokenEmulatorInterface
+final class NullsafeTokenEmulator implements TokenEmulatorInterface
 {
     public function getPhpVersion(): string
     {
-        return Emulative::PHP_7_4;
+        return Emulative::PHP_8_0;
     }
 
     public function isEmulationNeeded(string $code): bool
     {
-        return strpos($code, '??=') !== false;
+        return strpos($code, '?->') !== false;
     }
 
     public function emulate(string $code, array $tokens): array
@@ -23,9 +23,9 @@ final class CoaleseEqualTokenEmulator implements TokenEmulatorInterface
         $line = 1;
         for ($i = 0, $c = count($tokens); $i < $c; ++$i) {
             if (isset($tokens[$i + 1])) {
-                if ($tokens[$i][0] === T_COALESCE && $tokens[$i + 1] === '=') {
+                if ($tokens[$i] === '?' && $tokens[$i + 1][0] === \T_OBJECT_OPERATOR) {
                     array_splice($tokens, $i, 2, [
-                        [\T_COALESCE_EQUAL, '??=', $line]
+                        [\T_NULLSAFE_OBJECT_OPERATOR, '?->', $line]
                     ]);
                     $c--;
                     continue;
@@ -41,7 +41,7 @@ final class CoaleseEqualTokenEmulator implements TokenEmulatorInterface
 
     public function reverseEmulate(string $code, array $tokens): array
     {
-        // ??= was not valid code previously, don't bother.
+        // ?-> was not valid code previously, don't bother.
         return $tokens;
     }
 }
