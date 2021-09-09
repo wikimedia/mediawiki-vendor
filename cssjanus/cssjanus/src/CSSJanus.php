aa76f902bc3321063579ba003b3268b5db19ec41
James D. Forrester <jforrester@wikimedia.org>
2021-09-09 12:43:01 -0700

    Update cssjanus/cssjanus from 2.0.0 to 2.1.0
    Change-Id: Iac0b7702b001062495f1e08d055481d3c436161e

--- cssjanus/cssjanus/src/CSSJanus.php
+++ cssjanus/cssjanus/src/CSSJanus.php
@@ -30,67 +30,63 @@ class CSSJanus {
 	private const TOKEN_TMP = '`TMP`';
 	private const TOKEN_COMMENT = '`COMMENT`';
 
-	// Patterns defined as null are built dynamically by buildPatterns()
-	private static $patterns = array(
-		'tmpToken' => '`TMP`',
-		'nonAscii' => '[\200-\377]',
-		'unicode' => '(?:(?:\\\\[0-9a-f]{1,6})(?:\r\n|\s)?)',
-		'num' => '(?:[0-9]*\.[0-9]+|[0-9]+)',
-		'unit' => '(?:em|ex|px|cm|mm|in|pt|pc|deg|rad|grad|ms|s|hz|khz|%)',
-		'body_selector' => 'body\s*{\s*',
-		'direction' => 'direction\s*:\s*',
-		'escape' => null,
-		'nmstart' => null,
-		'nmchar' => null,
-		'ident' => null,
-		'quantity' => null,
-		'possibly_negative_quantity' => null,
-		'color' => null,
-		'url_special_chars' => '[!#$%&*-~]',
-		'valid_after_uri_chars' => '[\'\"]?\s*',
-		'url_chars' => null,
-		'lookahead_not_open_brace' => null,
-		'lookahead_not_closing_paren' => null,
-		'lookahead_for_closing_paren' => null,
-		'lookahead_not_letter' => '(?![a-zA-Z])',
-		'lookbehind_not_letter' => '(?<![a-zA-Z])',
-		'chars_within_selector' => '[^\}]*?',
-		'noflip_annotation' => '\/\*\!?\s*@noflip\s*\*\/',
-		'noflip_single' => null,
-		'noflip_class' => null,
-		'comment' => '/\/\*[^*]*\*+([^\/*][^*]*\*+)*\//',
-		'direction_ltr' => null,
-		'direction_rtl' => null,
-		'left' => null,
-		'right' => null,
-		'left_in_url' => null,
-		'right_in_url' => null,
-		'ltr_in_url' => null,
-		'rtl_in_url' => null,
-		'cursor_east' => null,
-		'cursor_west' => null,
-		'four_notation_quantity' => null,
-		'four_notation_color' => null,
-		'border_radius' => null,
-		'box_shadow' => null,
-		'text_shadow1' => null,
-		'text_shadow2' => null,
-		'bg_horizontal_percentage' => null,
-		'bg_horizontal_percentage_x' => null,
-		'suffix' => '(\s*(?:!important\s*)?[;}])'
-	);
+	private static $patterns = null;
 
-	/**
-	 * Build patterns we can't define above because they depend on other patterns.
-	 */
 	private static function buildPatterns() {
-		if (!is_null(self::$patterns['escape'])) {
-			// Patterns have already been built
+		if (self::$patterns !== null) {
 			return;
 		}
+		// Patterns defined as null are built dynamically
+		$patterns = array(
+			'tmpToken' => '`TMP`',
+			'nonAscii' => '[\200-\377]',
+			'unicode' => '(?:(?:\\\\[0-9a-f]{1,6})(?:\r\n|\s)?)',
+			'num' => '(?:[0-9]*\.[0-9]+|[0-9]+)',
+			'unit' => '(?:em|ex|px|cm|mm|in|pt|pc|deg|rad|grad|ms|s|hz|khz|%)',
+			'body_selector' => 'body\s*{\s*',
+			'direction' => 'direction\s*:\s*',
+			'escape' => null,
+			'nmstart' => null,
+			'nmchar' => null,
+			'ident' => null,
+			'quantity' => null,
+			'possibly_negative_quantity' => null,
+			'color' => null,
+			'url_special_chars' => '[!#$%&*-~]',
+			'valid_after_uri_chars' => '[\'\"]?\s*',
+			'url_chars' => null,
+			'lookahead_not_open_brace' => null,
+			'lookahead_not_closing_paren' => null,
+			'lookahead_for_closing_paren' => null,
+			'lookahead_not_letter' => '(?![a-zA-Z])',
+			'lookbehind_not_letter' => '(?<![a-zA-Z])',
+			'chars_within_selector' => '[^\}]*?',
+			'noflip_annotation' => '\/\*\!?\s*@noflip\s*\*\/',
+			'noflip_single' => null,
+			'noflip_class' => null,
+			'comment' => '/\/\*[^*]*\*+([^\/*][^*]*\*+)*\//',
+			'direction_ltr' => null,
+			'direction_rtl' => null,
+			'left' => null,
+			'right' => null,
+			'left_in_url' => null,
+			'right_in_url' => null,
+			'ltr_in_url' => null,
+			'rtl_in_url' => null,
+			'cursor_east' => null,
+			'cursor_west' => null,
+			'four_notation_quantity' => null,
+			'four_notation_color' => null,
+			'border_radius' => null,
+			'box_shadow' => null,
+			'text_shadow1' => null,
+			'text_shadow2' => null,
+			'bg_horizontal_percentage' => null,
+			'bg_horizontal_percentage_x' => null,
+			'suffix' => '(\s*(?:!important\s*)?[;}])'
+		);
 
 		// @codingStandardsIgnoreStart Generic.Files.LineLength.TooLong
-		$patterns =& self::$patterns;
 		$patterns['escape'] = "(?:{$patterns['unicode']}|\\\\[^\\r\\n\\f0-9a-f])";
 		$patterns['nmstart'] = "(?:[_a-z]|{$patterns['nonAscii']}|{$patterns['escape']})";
 		$patterns['nmchar'] = "(?:[_a-z0-9-]|{$patterns['nonAscii']}|{$patterns['escape']})";
@@ -101,7 +97,7 @@ class CSSJanus {
 		// Use "*+" instead of "*?" to avoid reaching the backtracking limit.
 		// <https://github.com/cssjanus/php-cssjanus/issues/14>, <https://phabricator.wikimedia.org/T215746#4944830>.
 		$patterns['url_chars'] = "(?:{$patterns['url_special_chars']}|{$patterns['nonAscii']}|{$patterns['escape']})*+";
-		$patterns['lookahead_not_open_brace'] = "(?!({$patterns['nmchar']}|\\r?\\n|\s|#|\:|\.|\,|\+|>|\(|\)|\[|\]|=|\*=|~=|\^=|'[^']*'|\"[^\"]*\"|" . self::TOKEN_COMMENT . ")*+{)";
+		$patterns['lookahead_not_open_brace'] = "(?!({$patterns['nmchar']}|\\r?\\n|\s|#|\:|\.|\,|\+|>|~|\(|\)|\[|\]|=|\*=|~=|\^=|'[^']*'|\"[^\"]*\"|" . self::TOKEN_COMMENT . ")*+{)";
 		$patterns['lookahead_not_closing_paren'] = "(?!{$patterns['url_chars']}{$patterns['valid_after_uri_chars']}\))";
 		$patterns['lookahead_for_closing_paren'] = "(?={$patterns['url_chars']}{$patterns['valid_after_uri_chars']}\))";
 		$patterns['noflip_single'] = "/({$patterns['noflip_annotation']}{$patterns['lookahead_not_open_brace']}[^;}]+;?)/i";
@@ -133,14 +129,19 @@ class CSSJanus {
 		$patterns['translate_x'] = "/(transform\s*:[^;}]*)(translateX\s*\(\s*){$patterns['possibly_negative_quantity']}(\s*\))/i";
 		$patterns['translate'] = "/(transform\s*:[^;}]*)(translate\s*\(\s*){$patterns['possibly_negative_quantity']}((?:\s*,\s*{$patterns['possibly_negative_quantity']}){0,2}\s*\))/i";
 		// @codingStandardsIgnoreEnd
+
+		self::$patterns = $patterns;
 	}
+
 
 	/**
 	 * Transform an LTR stylesheet to RTL
+	 *
 	 * @param string $css Stylesheet to transform
-	 * @param array|bool $options Options array or value of transformDirInUrl option (back-compat)
-	 * @param bool $options['transformDirInUrl'] Transform directions in URLs (ltr/rtl). Default: false.
-	 * @param bool $options['transformEdgeInUrl'] Transform edges in URLs (left/right). Default: false.
+	 * @param bool|array{transformDirInUrl?:bool,transformEdgeInUrl?:bool} $options Options array,
+	 * or value of transformDirInUrl option (back-compat)
+	 *  - transformDirInUrl: Transform directions in URLs (ltr/rtl). Default: false.
+	 *  - transformEdgeInUrl: Transform edges in URLs (left/right). Default: false.
 	 * @param bool $transformEdgeInUrl [optional] For back-compat
 	 * @return string Transformed stylesheet
 	 */
@@ -158,12 +159,12 @@ class CSSJanus {
 			'transformEdgeInUrl' => false,
 		);
 
+		self::buildPatterns();
+
 		// We wrap tokens in ` , not ~ like the original implementation does.
 		// This was done because ` is not a legal character in CSS and can only
 		// occur in URLs, where we escape it to %60 before inserting our tokens.
 		$css = str_replace('`', '%60', $css);
-
-		self::buildPatterns();
 
 		// Tokenize single line rules with /* @noflip */
 		$noFlipSingle = new CSSJanusTokenizer(self::$patterns['noflip_single'], '`NOFLIP_SINGLE`');
