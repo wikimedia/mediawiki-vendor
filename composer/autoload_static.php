13aa9ee993fa97410c05e377ea7be4e25d55e8bf
Reedy <reedy@wikimedia.org>
2022-03-25 13:00:16 +0000

    Update symfony/*
      - Upgrading symfony/console (v5.4.2 => v5.4.5)
      - Upgrading symfony/process (v5.4.3 => v5.4.5)
      - Upgrading symfony/translation (v5.4.3 => v5.4.6)
      - Upgrading symfony/validator (v5.4.3 => v5.4.6)
      - Upgrading symfony/var-dumper (v5.4.3 => v5.4.6)
    
    Change-Id: I789eedb1f785ab211e0ff9abe4bc1ead9302de3d

--- composer/autoload_static.php
+++ composer/autoload_static.php
@@ -3491,6 +3491,7 @@ class ComposerStaticInit_mediawiki_vendor
         'Symfony\\Component\\VarDumper\\Caster\\IntlCaster' => __DIR__ . '/..' . '/symfony/var-dumper/Caster/IntlCaster.php',
         'Symfony\\Component\\VarDumper\\Caster\\LinkStub' => __DIR__ . '/..' . '/symfony/var-dumper/Caster/LinkStub.php',
         'Symfony\\Component\\VarDumper\\Caster\\MemcachedCaster' => __DIR__ . '/..' . '/symfony/var-dumper/Caster/MemcachedCaster.php',
+        'Symfony\\Component\\VarDumper\\Caster\\MysqliCaster' => __DIR__ . '/..' . '/symfony/var-dumper/Caster/MysqliCaster.php',
         'Symfony\\Component\\VarDumper\\Caster\\PdoCaster' => __DIR__ . '/..' . '/symfony/var-dumper/Caster/PdoCaster.php',
         'Symfony\\Component\\VarDumper\\Caster\\PgSqlCaster' => __DIR__ . '/..' . '/symfony/var-dumper/Caster/PgSqlCaster.php',
         'Symfony\\Component\\VarDumper\\Caster\\ProxyManagerCaster' => __DIR__ . '/..' . '/symfony/var-dumper/Caster/ProxyManagerCaster.php',
