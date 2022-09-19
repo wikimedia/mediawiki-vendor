f94778cadd71418c0a28e81a73e2fee0e96de7d3
wfan <wfan@wikimedia.org>
2022-09-19 14:31:02 -0700

    update smash-pig requirement to 0.8.2.12
    Change-Id: Icd4564eabe346eb24242bbd2a4703ab335237f79

--- wikimedia/smash-pig/PaymentProviders/Adyen/Audit/AdyenAudit.php
+++ wikimedia/smash-pig/PaymentProviders/Adyen/Audit/AdyenAudit.php
@@ -10,44 +10,12 @@ use SmashPig\PaymentProviders\Adyen\ReferenceData;
 /**
  * Class AdyenAudit
  * @package SmashPig\PaymentProviders\Adyen\Audit
- * Processes Adyen's Settlement Detail Reports.
+ * Processes Adyen's Settlement Detail and Payments Accounting Reports.
  * Sends donations, chargebacks, and refunds to queue.
  * https://docs.adyen.com/manuals/reporting-manual/settlement-detail-report-structure/settlement-detail-report-journal-types
+ * https://docs.adyen.com/reporting/invoice-reconciliation/payment-accounting-report
  */
-class AdyenAudit implements AuditParser {
-
-	protected $columnHeaders = [
-		'Company Account',
-		'Merchant Account',
-		'Psp Reference',
-		'Merchant Reference',
-		'Payment Method',
-		'Creation Date',
-		'TimeZone',
-		'Type',
-		'Modification Reference',
-		'Gross Currency',
-		'Gross Debit (GC)',
-		'Gross Credit (GC)',
-		'Exchange Rate',
-		'Net Currency',
-		'Net Debit (NC)',
-		'Net Credit (NC)',
-		'Commission (NC)',
-		'Markup (NC)',
-		'Scheme Fees (NC)',
-		'Interchange (NC)',
-		'Payment Method Variant',
-		'Modification Merchant Reference',
-		'Batch Number',
-		'Reserved4',
-		'Reserved5',
-		'Reserved6',
-		'Reserved7',
-		'Reserved8',
-		'Reserved9',
-		'Reserved10',
-	];
+abstract class AdyenAudit implements AuditParser {
 
 	protected static $ignoredTypes = [
 		'fee',
@@ -69,9 +37,27 @@ class AdyenAudit implements AuditParser {
 		'paidout',
 		'paidoutreversed',
 		'reserveadjustment',
+		// payments accounting report ignored types
+		'received',
+		'authorised',
+		'sentforsettle',
+		'refused',
+		'retried',
+		'reversed',
+		'cancelled',
+		'sentforrefund', // does this eventually become refunded
+		'settledexternally' // what does this mean
+
 	];
 
 	protected $fileData;
+	// the state of the payment in the report
+	protected $type;
+	protected $columnHeaders;
+
+	abstract protected function parseDonation( array $row, array $msg );
+
+	abstract protected function parseRefund( array $row, array $msg );
 
 	public function parseFile( string $path ): array {
 		$this->fileData = [];
@@ -97,73 +83,39 @@ class AdyenAudit implements AuditParser {
 
 	protected function parseLine( $line ) {
 		$row = array_combine( $this->columnHeaders, $line );
-		$type = strtolower( $row['Type'] );
+		$type = strtolower( $row[$this->type] );
 		if ( in_array( $type, self::$ignoredTypes ) ) {
 			return;
 		}
 
-		$msg = [
-			'gateway' => 'adyen',
-			'gateway_account' => $row['Merchant Account'],
-			'invoice_id' => $row['Merchant Reference'],
-			'date' => $this->getDate( $row ),
-		];
-		$parts = explode( '.', $row['Merchant Reference'] );
-		$msg['contribution_tracking_id'] = $parts[0];
+		// maybe just put this back then
+		$msg = $this->setCommonValues( $row );
 
 		switch ( $type ) {
 			case 'settled':
-				$this->parseDonation( $row, $msg );
+				$msg = $this->parseDonation( $row, $msg );
 				break;
 			case 'chargeback':
 			case 'refunded':
-				$this->parseRefund( $row, $msg );
+				$msg = $this->parseRefund( $row, $msg );
 				break;
 			default:
-				throw new OutOfBoundsException( "Unknown audit line type {$row['Type']}." );
+				throw new OutOfBoundsException( "Unknown audit line type {$this->type}." );
 		}
 
 		$this->fileData[] = $msg;
 	}
 
-	protected function parseRefund( array $row, array &$msg ) {
-		$msg['gross'] = $row['Gross Debit (GC)']; // Actually paid to donor
-		$msg['gross_currency'] = $row['Gross Currency'];
-		// 'Net Debit (NC)' is the amount we paid including fees
-		// 'Net Currency' is the currency we paid in
-		// Deal with these when queue consumer can understand them
-
-		$msg['gateway_parent_id'] = $row['Psp Reference'];
-		$msg['gateway_refund_id'] = $row['Modification Reference'];
-		if ( strtolower( $row['Type'] ) === 'chargeback' ) {
-			$msg['type'] = 'chargeback';
-		} else {
-			$msg['type'] = 'refund';
-		}
-	}
-
-	protected function parseDonation( array $row, array &$msg ) {
-		$msg['gateway_txn_id'] = $row['Psp Reference'];
-		// T306944
-		// We were saving the Capture ID for 2+ recurrings in civi which for settled payments is in the
-		// Modification reference. Adding this lets us match donations until the data is cleaned up
-		$msg['modification_reference'] = $row['Modification Reference'];
-
-		$msg['currency'] = $row['Gross Currency'];
-		$msg['gross'] = $row['Gross Credit (GC)'];
-		// fee is given in settlement currency
-		// but queue consumer expects it in original
-		$exchange = $row['Exchange Rate'];
-		$fee = floatval( $row['Commission (NC)'] ) +
-			floatval( $row['Markup (NC)'] ) +
-			floatval( $row['Scheme Fees (NC)'] ) +
-			floatval( $row['Interchange (NC)'] );
-		$msg['fee'] = round( $fee / $exchange, 2 );
-
-		// shouldn't this be settled_net or settled_amount?
-		$msg['settled_gross'] = $row['Net Credit (NC)'];
-		$msg['settled_currency'] = $row['Net Currency'];
-		$msg['settled_fee'] = $fee;
+	// these column names are shared between SettlementDetail and PaymentsAccounting reports
+	protected function setCommonValues( $row ) {
+		$msg = [
+			'gateway' => 'adyen',
+			'gateway_account' => $row['Merchant Account'],
+			'invoice_id' => $row['Merchant Reference'],
+			'gateway_txn_id' => $row['Psp Reference']
+		];
+		$parts = explode( '.', $row['Merchant Reference'] );
+		$msg['contribution_tracking_id'] = $parts[0];
 
 		list( $method, $submethod ) = ReferenceData::decodePaymentMethod(
 			$row['Payment Method'],
@@ -171,11 +123,11 @@ class AdyenAudit implements AuditParser {
 		);
 		$msg['payment_method'] = $method;
 		$msg['payment_submethod'] = $submethod;
+		// Both reports have the Creation Date in PDT, the payments accounting report does not
+		// send the timezone as a separate column
+		$msg['date'] = UtcDate::getUtcTimestamp( $row['Creation Date'], 'PDT' );
+
+		return $msg;
 	}
 
-	protected function getDate( $row ) {
-		$local = $row['Creation Date'];
-		$zone = $row['TimeZone'];
-		return UtcDate::getUtcTimestamp( $local, $zone );
-	}
 }
