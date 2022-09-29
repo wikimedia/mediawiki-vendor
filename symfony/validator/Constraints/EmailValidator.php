a8880aebebceb3130cc1a11a163ce63c1125f582
Reedy <reedy@wikimedia.org>
2022-09-30 00:21:30 +0100

    Update symfony/*
      - Upgrading symfony/console (v5.4.10 => v5.4.12)
      - Upgrading symfony/deprecation-contracts (v2.5.1 => v2.5.2)
      - Upgrading symfony/process (v5.4.8 => v5.4.11)
      - Upgrading symfony/string (v5.4.10 => v5.4.12)
      - Upgrading symfony/translation (v5.4.9 => v5.4.12)
      - Upgrading symfony/translation-contracts (v2.5.1 => v2.5.2)
      - Upgrading symfony/validator (v5.4.10 => v5.4.12)
      - Upgrading symfony/var-dumper (v5.4.9 => v5.4.11)
    
    Change-Id: I13a261d6ca07e48ec07415b90c07639eef40ffa9

--- symfony/validator/Constraints/EmailValidator.php
+++ symfony/validator/Constraints/EmailValidator.php
@@ -56,7 +56,7 @@ class EmailValidator extends ConstraintValidator
             return;
         }
 
-        if (!is_scalar($value) && !(\is_object($value) && method_exists($value, '__toString'))) {
+        if (!\is_scalar($value) && !(\is_object($value) && method_exists($value, '__toString'))) {
             throw new UnexpectedValueException($value, 'string');
         }
 
@@ -70,6 +70,10 @@ class EmailValidator extends ConstraintValidator
         }
 
         if (null === $constraint->mode) {
+            if (Email::VALIDATION_MODE_STRICT === $this->defaultMode && !class_exists(EguliasEmailValidator::class)) {
+                throw new LogicException(sprintf('The "egulias/email-validator" component is required to make the "%s" constraint default to strict mode.', EguliasEmailValidator::class));
+            }
+
             $constraint->mode = $this->defaultMode;
         }
 
