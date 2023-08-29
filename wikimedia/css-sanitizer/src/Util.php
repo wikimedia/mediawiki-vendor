cb008e4dabe13e3b75fab117911660d793081fcf
Derick Alangi <alangiderick@gmail.com>
2023-08-29 16:28:59 +0100

    Update wikimedia/css-sanitizer from 4.0.1 -> 5.0.0
    Bug: T287972
    Change-Id: I7e0fb989236df696af3d4a0a1e7b3ab4d12fe571

--- wikimedia/css-sanitizer/src/Util.php
+++ wikimedia/css-sanitizer/src/Util.php
@@ -82,7 +82,7 @@ class Util {
 	/**
 	 * Turn a CSSObject into a string
 	 * @param CSSObject|CSSObject[] $object
-	 * @param array $options Serialziation options:
+	 * @param array $options Serialization options:
 	 *  - minify: (bool) Skip comments and insignificant tokens
 	 * @return string
 	 */
@@ -123,6 +123,7 @@ class Util {
 			// Avoid serializing tokens that are part of a <urange> with extraneous comments
 			// by checking for a hack-flag in the type.
 			// @see Wikimedia\CSS\Matcher\UrangeMatcher
+			// @phan-suppress-next-line PhanAccessMethodInternal
 			$urangeHack = max( $urangeHack, $prev->urangeHack() );
 
 			if ( --$urangeHack <= 0 && Token::separate( $prev, $token ) ) {
