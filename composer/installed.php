368932654e1a00fe151531165f7f90fb55fe8a61
James D. Forrester <jforrester@wikimedia.org>
2023-10-03 17:12:50 -0400

    build: Update now that CI runs Composer 2.6.4 up from 2.5.1
    Bug: T347360
    Change-Id: I34886d90086f9ec945130cf59bc1b0b0a8f486ba

--- composer/installed.php
+++ composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'af30f13eb855f69df22731ad9e1b9b58a2ad676d',
         'type' => 'library',
         'install_path' => __DIR__ . '/../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'af30f13eb855f69df22731ad9e1b9b58a2ad676d',
             'type' => 'library',
             'install_path' => __DIR__ . '/../',
             'aliases' => array(),
