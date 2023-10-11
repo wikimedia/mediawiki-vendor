055aeca280d6037bc0fb19de55ec6b203da06455
C. Scott Ananian <cscott@cscott.net>
2023-10-10 21:54:05 -0400

    Bump wikimedia/parsoid to v0.18.0-a29
    This is the base of Parsoid's REL1_41 branch, which will become the
    stable 0.18.0 versions of Parsoid.
    
    Change-Id: Ifc0b7b064c08f0067081ca506ed734757c345f51

--- composer/installed.php
+++ composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => 'dev-master',
-        'version' => 'dev-master',
-        'reference' => 'f47801d740194402927ab383f25bcbe163050de6',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => NULL,
         'type' => 'library',
         'install_path' => __DIR__ . '/../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
-            'reference' => 'f47801d740194402927ab383f25bcbe163050de6',
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => NULL,
             'type' => 'library',
             'install_path' => __DIR__ . '/../',
             'aliases' => array(),
@@ -524,9 +524,9 @@
             'dev_requirement' => false,
         ),
         'wikimedia/parsoid' => array(
-            'pretty_version' => 'v0.18.0-a28',
-            'version' => '0.18.0.0-alpha28',
-            'reference' => '1634bbad156ad17c7855485efe0213bad4478a4c',
+            'pretty_version' => 'v0.18.0-a29',
+            'version' => '0.18.0.0-alpha29',
+            'reference' => '635c46f8bc7fc99cea9822acb5ae13516448954c',
             'type' => 'library',
             'install_path' => __DIR__ . '/../wikimedia/parsoid',
             'aliases' => array(),
