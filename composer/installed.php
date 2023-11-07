83e612b8529f7707ea50c3f98453c8cac20713e2
C. Scott Ananian <cscott@cscott.net>
2023-11-07 11:33:28 -0500

    Bump wikimedia/parsoid to v0.18.0
    This is a non-alpha release of Parsoid for MW 1.41.
    
    Change-Id: Ief2bcb0fbde1c134a3f350e3a7313dcc281dd3c6

--- composer/installed.php
+++ composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '055aeca280d6037bc0fb19de55ec6b203da06455',
         'type' => 'library',
         'install_path' => __DIR__ . '/../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '055aeca280d6037bc0fb19de55ec6b203da06455',
             'type' => 'library',
             'install_path' => __DIR__ . '/../',
             'aliases' => array(),
@@ -524,9 +524,9 @@
             'dev_requirement' => false,
         ),
         'wikimedia/parsoid' => array(
-            'pretty_version' => 'v0.18.0-a29',
-            'version' => '0.18.0.0-alpha29',
-            'reference' => '635c46f8bc7fc99cea9822acb5ae13516448954c',
+            'pretty_version' => 'v0.18.0',
+            'version' => '0.18.0.0',
+            'reference' => 'cc05e71e9d09c5c49cf353e1fba7c789fd863709',
             'type' => 'library',
             'install_path' => __DIR__ . '/../wikimedia/parsoid',
             'aliases' => array(),
