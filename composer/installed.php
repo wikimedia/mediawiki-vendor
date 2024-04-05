f66808fb5642d0597203f2efcc6a17006983937e
Reedy <reedy@wikimedia.org>
2024-04-05 16:16:31 +0100

    Rebuild with composer 2.7.2
    Bug: T360973
    Change-Id: I1b56610ed765cf0ea0eb77ec6470135a20bf2ef2

--- composer/installed.php
+++ composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '8dcc01f9c24507894245962612a231a859e9af92',
+        'reference' => '3906875b649f7b092a557d780f1ad24be26b439d',
         'type' => 'library',
         'install_path' => __DIR__ . '/../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '8dcc01f9c24507894245962612a231a859e9af92',
+            'reference' => '3906875b649f7b092a557d780f1ad24be26b439d',
             'type' => 'library',
             'install_path' => __DIR__ . '/../',
             'aliases' => array(),
