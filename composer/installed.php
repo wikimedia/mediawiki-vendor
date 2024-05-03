a985ea577d437509ee0e83673e336d2bed9b2a96
Alexander Vorwerk <zabe@avorwerk.net>
2024-05-03 22:53:44 +0200

    Update php-parallel-lint/php-parallel-lint to 1.4.0
    Change-Id: I7c482388884d3c9b56d7fa6e525d672ab55b7c93

--- composer/installed.php
+++ composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '9effd595984532bcde1a50d1664782d345cacca2',
+        'reference' => 'ad293fd058f517476539b9530bf77ca44f97cd53',
         'type' => 'library',
         'install_path' => __DIR__ . '/../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '9effd595984532bcde1a50d1664782d345cacca2',
+            'reference' => 'ad293fd058f517476539b9530bf77ca44f97cd53',
             'type' => 'library',
             'install_path' => __DIR__ . '/../',
             'aliases' => array(),
