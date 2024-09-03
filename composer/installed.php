dedd02cc9bb66dbfd33a330459ea24394759dd29
James D. Forrester <jforrester@wikimedia.org>
2024-09-03 09:06:58 -0400

    Follow-up 58bb1f9e1: Change build script image to one with composer 2.7.2, not 2.6.4
    Whoops. Thankfully not a practical change for our purposes.
    
    Bug: T360973
    Change-Id: I90e84f2f29f28228a52042e5f4b659d4fc88df7b

--- composer/installed.php
+++ composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'db0dd40abab76f62092af50923efb977d9d496da',
         'type' => 'library',
         'install_path' => __DIR__ . '/../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'db0dd40abab76f62092af50923efb977d9d496da',
             'type' => 'library',
             'install_path' => __DIR__ . '/../',
             'aliases' => array(),
