5a1ede1936764012880bdd903af892d854e4d186
James D. Forrester <jforrester@wikimedia.org>
2024-10-15 09:26:42 -0400

    Upgrade php-session-serializer from 2.0.1 to 3.0.0
    Breaking change is dropped PHP 7.4 support.
    
    Change-Id: Ice6ed4d3fa376a37faae75d11cb81c9221f7f29f

--- composer/autoload_classmap.php
+++ composer/autoload_classmap.php
@@ -1496,7 +1496,7 @@ return array(
     'Wikimedia\\Parsoid\\Wt2Html\\TreeBuilder\\TreeMutationRelay' => $vendorDir . '/wikimedia/parsoid/src/Wt2Html/TreeBuilder/TreeMutationRelay.php',
     'Wikimedia\\Parsoid\\Wt2Html\\Wt2HtmlDOMProcessor' => $vendorDir . '/wikimedia/parsoid/src/Wt2Html/Wt2HtmlDOMProcessor.php',
     'Wikimedia\\Parsoid\\Wt2Html\\XMLSerializer' => $vendorDir . '/wikimedia/parsoid/src/Wt2Html/XMLSerializer.php',
-    'Wikimedia\\PhpSessionSerializer' => $vendorDir . '/wikimedia/php-session-serializer/src/Wikimedia/PhpSessionSerializer.php',
+    'Wikimedia\\PhpSessionSerializer' => $vendorDir . '/wikimedia/php-session-serializer/src/PhpSessionSerializer.php',
     'Wikimedia\\Purtle\\BNodeLabeler' => $vendorDir . '/wikimedia/purtle/src/BNodeLabeler.php',
     'Wikimedia\\Purtle\\JsonLdRdfWriter' => $vendorDir . '/wikimedia/purtle/src/JsonLdRdfWriter.php',
     'Wikimedia\\Purtle\\N3Quoter' => $vendorDir . '/wikimedia/purtle/src/N3Quoter.php',
