2e0bfb9d91126849612308f9e910682fc4ad2229
Reedy <reedy@wikimedia.org>
2025-06-08 10:16:03 +0100

    composer.json: Bump to php >= 8.1.0
    Bug: T328921
    Change-Id: I59dd61b558fd33d30e1b4050616434f851fa867f

--- composer/installed.php
+++ composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'f7c5543aae7676465de9e3e799ec14496cba83d7',
         'type' => 'library',
         'install_path' => __DIR__ . '/../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'f7c5543aae7676465de9e3e799ec14496cba83d7',
             'type' => 'library',
             'install_path' => __DIR__ . '/../',
             'aliases' => array(),
