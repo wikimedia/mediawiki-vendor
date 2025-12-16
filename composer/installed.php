0ad45c8afa0c180cc6f61603f7a15faf78132f00
Timo Tijhof <krinkle@fastmail.com>
2025-12-16 22:08:05 +0000

    Upgrade pear/pear-core-minimal to v1.10.17
    This release includes my upstream patch for T379445 to fix a PHP 8.4
    warning, ref https://github.com/pear/pear-core/issues/155.
    
    Bug: T386108
    Change-Id: I9783a49e921de25b256e48be6d6e0554dd53fc82

--- composer/installed.php
+++ composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '82a4a17d3b71358f0311de56699a66d94ce60ef0',
+        'reference' => 'fa8de5afe124d6039bc74bfb6434e71bdfcd673e',
         'type' => 'library',
         'install_path' => __DIR__ . '/../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '82a4a17d3b71358f0311de56699a66d94ce60ef0',
+            'reference' => 'fa8de5afe124d6039bc74bfb6434e71bdfcd673e',
             'type' => 'library',
             'install_path' => __DIR__ . '/../',
             'aliases' => array(),
@@ -245,9 +245,9 @@
             'dev_requirement' => false,
         ),
         'pear/pear-core-minimal' => array(
-            'pretty_version' => 'v1.10.16',
-            'version' => '1.10.16.0',
-            'reference' => 'c0f51b45f50683bf5bbf558036854ebc9b54d033',
+            'pretty_version' => 'v1.10.17',
+            'version' => '1.10.17.0',
+            'reference' => 'c7b55789d01de0ce090d289b73f1bbd6a2f113b1',
             'type' => 'library',
             'install_path' => __DIR__ . '/../pear/pear-core-minimal',
             'aliases' => array(),
@@ -365,7 +365,7 @@
         'rsky/pear-core-min' => array(
             'dev_requirement' => false,
             'replaced' => array(
-                0 => 'v1.10.16',
+                0 => 'v1.10.17',
             ),
         ),
         'symfony/deprecation-contracts' => array(
