99dc1a29f8008f68b8b3aeb8790a3cfc174b5e04
Roan Kattouw <roan.kattouw@gmail.com>
2026-06-11 14:45:23 -0700

    Update OOUI to v0.54.0
      Release notes: https://gerrit.wikimedia.org/g/oojs/ui/+/v0.54.0/History.md
    
    Change-Id: I0450b63a103fd81b66ea6298ee00a469492d43af

--- composer/installed.php
+++ composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'ce7dfcc24279222470cbe6c6df62b24efb21f0b0',
         'type' => 'library',
         'install_path' => __DIR__ . '/../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'ce7dfcc24279222470cbe6c6df62b24efb21f0b0',
             'type' => 'library',
             'install_path' => __DIR__ . '/../',
             'aliases' => array(),
@@ -461,9 +461,9 @@
             'dev_requirement' => false,
         ),
         'oojs/oojs-ui' => array(
-            'pretty_version' => 'v0.53.2',
-            'version' => '0.53.2.0',
-            'reference' => '360097bdf5ade6b6ba622882de8b7625f1094868',
+            'pretty_version' => 'v0.54.0',
+            'version' => '0.54.0.0',
+            'reference' => 'b24073615e212250a6d8ee2f4e54eff79944c994',
             'type' => 'library',
             'install_path' => __DIR__ . '/../oojs/oojs-ui',
             'aliases' => array(),
