4b89ae2028ce6076a970fe165f4b7990fdbc2b50
Yiannis Giannelos <jgiannelos@wikimedia.org>
2026-06-15 17:11:56 +0200

    Bump guzzlehttp/psr to version 2.11.0
    Bug: T429208
    Change-Id: I7f618cf3703455cb3899582c96e0ca731eefc0f9

--- composer/autoload_files.php
+++ composer/autoload_files.php
@@ -6,8 +6,8 @@ $vendorDir = dirname(__DIR__);
 $baseDir = $vendorDir;
 
 return array(
-    '7b11c4dc42b3b3023073cb14e519683c' => $vendorDir . '/ralouphie/getallheaders/src/getallheaders.php',
     '6e3fae29631ef280660b3cdad06f25a8' => $vendorDir . '/symfony/deprecation-contracts/function.php',
+    '7b11c4dc42b3b3023073cb14e519683c' => $vendorDir . '/ralouphie/getallheaders/src/getallheaders.php',
     'c3f7f0e81464740a933532e81fa26cc7' => $vendorDir . '/wikimedia/base-convert/src/Functions.php',
     '37a3dc5111fe8f707ab4c132ef1dbc62' => $vendorDir . '/guzzlehttp/guzzle/src/functions_include.php',
     'c50606d667a3fde2b80a955639479d3d' => $vendorDir . '/wikimedia/timestamp/src/defines.php',
