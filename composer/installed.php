7407683676f8b7bb416dcc7d0dcb17e5deb3b51f
James D. Forrester <jforrester@wikimedia.org>
2026-07-02 14:30:52 +0100

    README: Update docker instruction to use image with composer 2.10.2
    This is a no-op beyond the usual churn in composer/installed.php, but
    I'm committing that anyway just for consistency with how we do these
    (and proof-of-life that I did run it with the new image!).
    
    Bug: T428570
    Change-Id: I5b5c7fc690931ac1e6d4558ad463b840176e9c20

--- composer/installed.php
+++ composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '0304d38d8f8a59f230231866d954804e3a1c99a4',
+        'reference' => '35424b257de4ee569592b67ff16dca596b359991',
         'type' => 'library',
         'install_path' => __DIR__ . '/../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '0304d38d8f8a59f230231866d954804e3a1c99a4',
+            'reference' => '35424b257de4ee569592b67ff16dca596b359991',
             'type' => 'library',
             'install_path' => __DIR__ . '/../',
             'aliases' => array(),
